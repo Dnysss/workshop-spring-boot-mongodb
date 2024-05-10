b213885d012d6e9dc8c34dfb1e8310b92390f6d4
Dnysss <denysantos2014@hotmail.com>
2024-05-10 14:49:34 -0300

    Simple query with query methods

--- src/main/java/com/dnys/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/dnys/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.dnys.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dnys.workshopmongo.domain.Post;
 
+@Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
